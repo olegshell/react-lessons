785357531ff893a05a1bf2c2789a3a4d3f0d7a09
olegshell <sheldonalik@gmail.com>
2022-01-12 15:56:41 +0300

    Lesson 19

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs'
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Settings from "./components/Settings/Settings";
+
+
 
 const App = (props) => {
     return (
